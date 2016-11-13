23eadd42a33d4d2ea12765b352fe45d86348dc3c
Flavia Almeida <fg_almeida@yahoo.com.br>
2016-11-12 23:57:22 -0200

    Versao 1.2 - Marca 				Insert funcionando 				Delete funcionando 				List funcionando

--- SmartCompras/src/main/java/org/smartcompras/controller/MarcaController.java
+++ SmartCompras/src/main/java/org/smartcompras/controller/MarcaController.java
@@ -1,6 +1,5 @@
 package org.smartcompras.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,44 +8,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.smartcompras.model.Marca;
 import org.smartcompras.repository.MarcaMongoRepository;
-
 
 @Controller
 public class MarcaController {
 
 	@Autowired
 	MarcaMongoRepository marcaRepository;
-	
 
-	
-	
-	@RequestMapping(value="/addmarcas", method=RequestMethod.POST)
-	public String addMarcas(@ModelAttribute Marca marca){
+	@RequestMapping(value = "/addmarcas", method = RequestMethod.POST)
+	public String addMarcas(@ModelAttribute Marca marca) {
 		marcaRepository.save(marca);
 		return "redirect:marca";
-		
+
 	}
-	
+
 	@RequestMapping(value = "/searchmarcas")
-    public String search(@RequestParam String searchmarcas) {
-        return "redirect:marca";
-    }
-	
+	public String search(@RequestParam String searchmarcas) {
+		return "redirect:marca";
+	}
+
 	@RequestMapping("/marca")
-    public String marcaList(Model model) {
-        model.addAttribute("marcaList", marcaRepository.findAll());
-        return "marca";
-    }
-	
-	
-	@RequestMapping("/delmarca")
-    public String delMarca(@ModelAttribute Marca marca) {
-		marcaRepository.delete(marca);
-//      	System.out.println(marca.getId());
-//      	System.out.println(marca.getMarca());
-        return "redirect:marca";
-    }
-	
+	public String marcaList(Model model) {
+		model.addAttribute("marcaList", marcaRepository.findAll());
+		return "marca";
+	}
+
+	@RequestMapping(value = "/delmarca", params = { "removerMarca" }, method = RequestMethod.POST)
+
+	public String delMarca(@ModelAttribute Marca marca, final HttpServletRequest req) {
+		String marcaId = String.valueOf((req.getParameter("removerMarca")));
+
+		marcaRepository.delete(marcaId);
+
+		System.out.println(marcaId);
+		return "redirect:marca";
+	}
+
 }
